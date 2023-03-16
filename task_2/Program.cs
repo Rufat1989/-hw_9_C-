8de9a86a08fb293c7ca885b066d7ba210237b8bc
Rufat Karimov <ruffat_karimov@mail.ru>
2023-03-16 11:36:57 +0600

    second task

---
+++ task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите N: ");
+int N = int.Parse(ReadLine());
+Write("Введите M: ");
+int M = int.Parse(ReadLine());
+
+SumNumbers(N, M);
+
+
+void SumNumbers(int N, int M)
+{
+    Write(Sum(N - 1, M));
+}
+
+int Sum(int N, int M)
+{
+    int res = N;
+    if (N == M)
+        return 0;
+    else
+    {
+        N++;
+        res = N + Sum(N, M);
+        return res;
+    }
+}
