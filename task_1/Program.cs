ea0dfc2222a8bfef70643db358ecf408f7e29476
Rufat Karimov <ruffat_karimov@mail.ru>
2023-03-16 11:13:49 +0600

    first task

---
+++ task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите start: ");
+int N = int.Parse(ReadLine());
+Write("Введите end: ");
+int M = int.Parse(ReadLine());
+
+WriteLine(PrintNumbers(N, M));
+
+string PrintNumbers(int start, int end)
+{
+    if (end == start)
+    {
+        Console.WriteLine(end);
+        return start.ToString();
+    }
+
+    string s = PrintNumbers(start, end + 1) + ' ' + end.ToString();
+    Console.WriteLine(s);
+    return (s);
+}
